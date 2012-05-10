131081827564b343c35982eb4360258fe2995ba9
Dorin GRIGORE <happylde@gmail.com>
2012-05-10 10:45:49 +0200

    Commented functions and changed some names

--- LDE/LDEfunctions.cpp
+++ LDE/LDEfunctions.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// Finds the next power of two number (used for OpenGL images for example)
 LDEuint LDEnextPot( LDEuint value )
 {
 	for ( LDEuint i = 2; i <= 8192; i *= 2 )
@@ -25,17 +26,19 @@ LDEuint LDEnextPot( LDEuint value )
 	return value;
 }
 
-// retourne un nombre alÈatoire entre minimum et maximum
+// Returns random double between min and max
 LDEdouble LDErand(LDEdouble min, LDEdouble max)
 {
 	return (LDEdouble) (min + ((LDEfloat) rand() / RAND_MAX * (max - min + 1.0)));
 }
 
+// Returns string with only the path of file (las slash excluded)
 std::string LDEpathOfFile(std::string filename)
 {
     return filename.substr( 0, filename.rfind("/") );
 }
 
+// Returns string with the name of file from a given path (extension excluded)
 std::string LDEnameOfFile(std::string filename)
 {
     string name_with_ext = filename.substr( filename.rfind("/") + 1 );
@@ -43,6 +46,7 @@ std::string LDEnameOfFile(std::string filename)
     return name_with_ext.substr( 0, name_with_ext.rfind(".") );
 }
 
+// Returns string with the extension of file from a given path (point excluded)
 std::string LDEextOfFile(std::string filename)
 {
     string name_with_ext = filename.substr( filename.rfind("/") + 1 );
@@ -50,20 +54,23 @@ std::string LDEextOfFile(std::string filename)
     return name_with_ext.substr( name_with_ext.rfind(".")+1 );
 }
 
-std::wstring StringToWString(const std::string& s)
+// Returns wstring from string (string to wstring)
+std::wstring LDEstws(const std::string& s)
 {
     std::wstring temp(s.length(),L' ');
     std::copy(s.begin(), s.end(), temp.begin());
     return temp;
 }
 
-std::string WStringToString(const std::wstring& s)
+// Returns string from wstring (wstring to string)
+std::string LDEwsts(const std::wstring& s)
 {
     std::string temp(s.length(), ' ');
     std::copy(s.begin(), s.end(), temp.begin());
     return temp;
 }
 
+// The awesome PHP function, replace string in string from string
 string& str_replace(const string &search, const string &replace, string &subject)
 {
     string buffer;
@@ -92,7 +99,7 @@ string& str_replace(const string &search, const string &replace, string &subject
     return subject;
 }
 
-// number to string
+// return string number (same as parseFloat)
 string LDEnts( LDEfloat n )
 {
     stringstream ss;
@@ -100,15 +107,15 @@ string LDEnts( LDEfloat n )
     return ss.str();
 }
 
-// number to wstring
+// return wstring number (same as parseFloat)
 wstring LDEntws( LDEfloat n )
 {
     stringstream ss;
     ss << n;
-    return StringToWString(ss.str());
+    return LDEstws(ss.str());
 }
 
-// string to number
+// Returns float number from given string
 LDEfloat LDEstn( string name )
 {
     LDEfloat temp = 0;
@@ -117,12 +124,14 @@ LDEfloat LDEstn( string name )
     return temp;
 }
 
-// string to char ( if we deal with situations like string+string+string to char
+// this function is only useful when you want to compose a full filepath from strings
+// eg: file.open( LDEstc(path+'/'+name+'.'+ext) );
 const char *LDEstc( string name )
 {
 	return name.c_str();
 }
 
+// Returns the distance between two 2D points
 LDEfloat LDedist2f(vec2f &start, vec2f &end)
 {
 	return sqrtf( ((start.x - end.x)*(start.x - end.x)) + ((start.y - end.y)*(start.y - end.y)) );
--- LDE/LDEfunctions.h
+++ LDE/LDEfunctions.h
@@ -21,28 +21,43 @@
 
 #include <iostream>
 
+// Finds the next power of two number (used for OpenGL images for example)
+LDEuint LDEnextPot( LDEuint value );
+
+// Returns random double between min and max
 LDEdouble LDErand(LDEdouble min, LDEdouble max);
 
-std::wstring StringToWString(const std::string& s);
+// Returns wstring from string (string to wstring)
+std::wstring LDEstws(const std::string& s);
 
-std::string WStringToString(const std::wstring& s);
+// Returns string from wstring (wstring to string)
+std::string LDEwsts(const std::wstring& s);
 
+// The awesome PHP function, replace string in string from string
 std::string& str_replace(const std::string &search, const std::string &replace, std::string &subject);
 
+// return string number (same as parseFloat)
 std::string LDEnts( LDEfloat n );
 
+// return wstring number (same as parseFloat)
 std::wstring LDEntws( LDEfloat n );
 
+// Returns string with only the path of file (las slash excluded)
 std::string LDEpathOfFile(std::string filename);
+
+// Returns string with the name of file from a given path (extension excluded)
 std::string LDEnameOfFile(std::string filename);
+
+// Returns string with the extension of file from a given path (point excluded)
 std::string LDEextOfFile(std::string filename);
 
+// Returns float number from given string
 LDEfloat LDEstn( std::string name );
 
+// eg: file.open( LDEstc(path+'/'+name+'.'+ext) );
 const char *LDEstc( std::string name );
 
+// Returns the distance between two 2D points
 LDEfloat LDEdist2f(vec2f &start, vec2f &end);
-
-LDEuint LDEnextPot( LDEuint value );
 
 #endif
